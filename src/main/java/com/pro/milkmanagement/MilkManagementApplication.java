b4afda0b1771e7d7f9af98fbb2753b930cdba350
sachin kumar <sachin200697@gmail.com>
2022-11-26 15:35:33 +0530

    solve cross-origin problem

--- src/main/java/com/pro/milkmanagement/MilkManagementApplication.java
+++ src/main/java/com/pro/milkmanagement/MilkManagementApplication.java
@@ -2,12 +2,27 @@ package com.pro.milkmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Controller
 public class MilkManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MilkManagementApplication.class, args);
+		SpringApplication.run(MilkManagementApplication.class, args);		
+	}
+	
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
